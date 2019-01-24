21e9c593bc67c7305c5cfd0ec6d023c6d4aa8a8a
Colin Gerety <colin@frii.com>
2019-01-24 10:05:22 -0700

    Fix event styling and require admin for supplier_items.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,6 @@ class Artist < ActiveRecord::Base
   default_scope { includes(:art_pieces).order("art_pieces.currently_hanging DESC, art_pieces.needs_label DESC, art_pieces.id DESC").references(:art_pieces) }
 
   has_many :art_pieces, dependent: :destroy, inverse_of: :artist
-  accepts_nested_attributes_for :art_pieces, allow_destroy: true, reject_if: proc {|attributes| attributes[:first].blank || attributes[:last].blank?}
+  accepts_nested_attributes_for :art_pieces, allow_destroy: true, reject_if: proc {|attributes| attributes[:first].blank? || attributes[:last].blank?}
 
 end
